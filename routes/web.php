fe1516feaa1f91a238d12bc8583bde6fc6b9a02c
Koramit Pichanaharee <koramit.p@gmail.com>
2020-12-24 19:41:59 +0700

    add create dude endpoint

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/dudes/{form}/{key}', function ($form, $key) {
+    Log::info(Request::all());
+    return 'ok';
 });
