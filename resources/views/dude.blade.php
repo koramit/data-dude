86ad7444fab4e1bf0b56a3426b145692d7ddd5a6
Koramit Pichanaharee <koramit.p@gmail.com>
2020-12-29 09:12:20 +0700

    test full data

--- resources/views/dude.blade.php
+++ resources/views/dude.blade.php
@@ -185,102 +185,193 @@
             data.append('allergy', extractValue(dom, 'input[name=Allergy][checked]', 'value'));
             // * Allergy detailt: input#Allergy_detail => value
             data.append('allergy_detail', extractValue(dom, 'input#Allergy_detail', 'value'));
-            console.table(data);
-            return data;
-
             // * no current medications: input#current_med => checked
+            data.append('no_current_medications', extractValue(dom, 'input#current_med', 'checked'));
             // * current medications: textarea#current_med_detail => innerText
+            data.append('current_med_detail', extractValue(dom, '#current_med_detail', 'innerText'));
             // * family: textarea#family => innerText
+            data.append('family', extractValue(dom, '#family', 'innerText'));
             // * personal social: textarea#personal_social => innerText
+            data.append('personal_social', extractValue(dom, '#personal_social', 'innerText'));
             // * General symtoms: textarea#gensym => innerText
+            data.append('general_symtoms', extractValue(dom, '#gensym', 'innerText'));
             // * review head: input[name=review_head][checked] => value
+            data.append('review_head', extractValue(dom, 'input[name=review_head][checked]', 'value'));
             // * review head detail: textarea#review_detail_head => innerText
+            data.append('review_detail_head', extractValue(dom, '#review_detail_head', 'innerText'));
             // * review eye: input[name=review_eye][checked] => value
+            data.append('review_eye_ENT', extractValue(dom, 'input[name=review_eye][checked]', 'value'));
             // * review eye detail: textarea#review_detail_eye => innerText
+            data.append('review_detail_eye_ENT', extractValue(dom, '#review_detail_eye', 'innerText'));
             // * review cvs: input[name=review_cvs][checked] => value
+            data.append('review_CVS', extractValue(dom, 'input[name=review_cvs][checked]', 'value'));
             // * review cvs detail: textarea#review_detail_cvs => innerText
+            data.append('review_detail_CVS', extractValue(dom, '#review_detail_cvs', 'innerText'));
             // * review RS: input[name=review_rs][checked] => value
+            data.append('review_RS', extractValue(dom, 'input[name=review_rs][checked]', 'value'));
             // * review RD detail: textarea#review_detail_RS => innerText
+            data.append('review_detail_RS', extractValue(dom, '#review_detail_RS', 'innerText'));
             // * review GI: input[name=review_GI][checked] => value
+            data.append('review_GI', extractValue(dom, 'input[name=review_GI][checked]', 'value'));
             // * review GI detail: textarea#review_detail_GI => innerText
+            data.append('review_detail_GI', extractValue(dom, '#review_detail_GI', 'innerText'));
             // * review GU: input[name=review_GU][checked] => value
+            data.append('review_GU', extractValue(dom, 'input[name=review_GU][checked]', 'value'));
             // * review GU detail: textarea#review_detail_GU => innerText
+            data.append('review_detail_GU', extractValue(dom, '#review_detail_GU', 'innerText'));
             // * review Musculoskeletal system: input[name=review_musculo][checked] => value
+            data.append('review_musculoskeletal_system', extractValue(dom, 'input[name=review_musculo][checked]',
+                'value'));
             // * review Musculoskeletal system detail: textarea#review_detail_Musc => innerText
+            data.append('review_detail_musculoskeletal_system', extractValue(dom, '#review_detail_Musc', 'innerText'));
             // * review Nervous system: input[name=review_nerv][checked] => value
+            data.append('review_nervous_system', extractValue(dom, 'input[name=review_nerv][checked]', 'value'));
             // * review Nervous system detail: textarea#review_detail_nerv => innerText
+            data.append('review_detail_nervous_system', extractValue(dom, '#review_detail_nerv', 'innerText'));
             // * review Psychological sysmptoms: input[name=review_psych][checked] => value
+            data.append('review_psychological_sysmptoms', extractValue(dom, 'input[name=review_psych][checked]',
+                'value'));
             // * review Psychological sysmptoms detail: textarea#review_detail_psyc => innerText
+            data.append('review_detail_psychological_sysmptoms', extractValue(dom, '#review_detail_psyc', 'innerText'));
             // * review other detail: textarea[name=review_other_text] => innerText
+            data.append('review_other', extractValue(dom, 'textarea[name=review_other_text]', 'innerText'));
             // * NG tube/NG suction: input[name=require_NG] => checked
+            data.append('require_NG_tube_NG_suction', extractValue(dom, 'input[name=require_NG]', 'checked'));
             // * Gastrostomy feeding: input[name=require_feeding] => checked
+            data.append('require_gastrostomy_feeding', extractValue(dom, 'input[name=require_feeding]', 'checked'));
             // * Urinary cath. care: input[name=require_urinary] => checked
+            data.append('require_urinary_cath_care', extractValue(dom, 'input[name=require_urinary]', 'checked'));
             // * Tracheostomy care: input[name=require_trache] => checked
+            data.append('require_tracheostomy_care', extractValue(dom, 'input[name=require_trache]', 'checked'));
             // * Hearing impairment: input[name=require_hearing] => checked
+            data.append('require_hearing_impairment', extractValue(dom, 'input[name=require_hearing]', 'checked'));
             // * Visiual impairment: input[name=require_visiual] => checked
+            data.append('require_visiual_impairment', extractValue(dom, 'input[name=require_visiual]', 'checked'));
             // * Isolation room: input[name=require_isolate] => checked
+            data.append('require_isolation_room', extractValue(dom, 'input[name=require_isolate]', 'checked'));
             // * Special Requiremen other: textarea[name=require_other_text] => innerText
+            data.append('require_isolation_room', extractValue(dom, 'textarea[name=require_other_text]', 'innerText'));
             // * temperature: input#vital_T => value
+            data.append('temperature', extractValue(dom, '#vital_T', 'value'));
             // * pulse: input#vital_P => value
+            data.append('pulse', extractValue(dom, '#vital_P', 'value'));
             // * raspiry rate: input#vital_R => value
+            data.append('raspiry rate', extractValue(dom, '#vital_R', 'value'));
             // * SBP: input#vital_BP1 => value
+            data.append('SBP', extractValue(dom, '#vital_BP1', 'value'));
             // * DBP: input#vital_BP2 => value
+            data.append('DBP', extractValue(dom, '#vital_BP2', 'value'));
             // * height: input#height => value
+            data.append('height', extractValue(dom, '#height', 'value'));
             // * weight: input#weight => value
+            data.append('weight', extractValue(dom, '#weight', 'value'));
             // * BMI: input#BMI => value
+            data.append('BMI', extractValue(dom, '#BMI', 'value'));
             // * spo2: input#spo2 => value
+            data.append('SPO2', extractValue(dom, '#spo2', 'value'));
             // * breathing: input[name=Room_type]=> value
+            data.append('breathing', extractValue(dom, 'input[name=Room_type]', 'value'));
             // * o2_type: input[name=via]=> value
+            data.append('O2_type', extractValue(dom, 'input[name=via]', 'value'));
             // * o2_rate: input#O2 => value
+            data.append('O2_rate', extractValue(dom, '#O2', 'value'));
             // * level of conscious: input[name=conscious]=> value
+            data.append('level_of_conscious', extractValue(dom, 'input[name=conscious]', 'value'));
             // * gcs_e: input#E => value
+            data.append('GCS_E', extractValue(dom, '#E', 'value'));
             // * gcs_v: input#V => value
+            data.append('GCS_V', extractValue(dom, '#V', 'value'));
             // * gcs_m: input#M => value
+            data.append('GCS_M', extractValue(dom, '#M', 'value'));
             // * gcs: input#glassgow_detail => value
+            data.append('glassgow_detail', extractValue(dom, '#glassgow_detail', 'value'));
             // * mental evaluation: input[name=mental]=> value
+            data.append('mental_evaluation', extractValue(dom, 'input[name=mental]', 'value'));
             // * orientation to time: input#orient_time => checked
+            data.append('orientation_to_time', extractValue(dom, '#orient_time', 'value'));
             // * orientation to place: input#orient_place => checked
+            data.append('orientation_to_place', extractValue(dom, '#orient_place', 'value'));
             // * orientation to person : input#orient_person => checked
+            data.append('orientation_to_person', extractValue(dom, '#orient_person', 'value'));
             // * General appearance: textarea#general_app => innerText
+            data.append('general_appearance', extractValue(dom, '#general_app', 'innerText'));
             // * exam skin: input[name=exam_skin][checked] => value
+            data.append('exam_skin', extractValue(dom, 'input[name=exam_skin][checked]', 'value'));
             // * exam skin detail: textarea#exam_skin_detail => innerText
+            data.append('exam_skin_detail', extractValue(dom, '#exam_skin_detail', 'innerText'));
             // * exam head face: input[name=exam_face][checked] => value
+            data.append('exam_head_face', extractValue(dom, 'input[name=exam_face][checked]', 'value'));
             // * exam head face detail: textarea#exam_face_detail => innerText
+            data.append('exam_head_face_detail', extractValue(dom, '#exam_face_detail', 'innerText'));
             // * exam eye ent: input[name=exam_eye][checked] => value
+            data.append('exam_eye_ENT', extractValue(dom, 'input[name=exam_eye][checked]', 'value'));
             // * exam eye ent detail: textarea#exam_eye_detail => innerText
+            data.append('exam_eye_ENT_detail', extractValue(dom, '#exam_eye_detail', 'innerText'));
             // * exam neck: input[name=exam_neck][checked] => value
+            data.append('exam_neck', extractValue(dom, 'input[name=exam_neck][checked]', 'value'));
             // * exam neck detail: textarea#exam_neck_detail => innerText
+            data.append('exam_neck_detail', extractValue(dom, '#exam_neck_detail', 'innerText'));
             // * exam heart: input[name=exam_heart][checked] => value
+            data.append('exam_heart', extractValue(dom, 'input[name=exam_heart][checked]', 'value'));
             // * exam heart detail: textarea#exam_heart_detail => innerText
+            data.append('exam_heart_detail', extractValue(dom, '#exam_heart_detail', 'innerText'));
             // * exam lungs: input[name=exam_lungs][checked] => value
+            data.append('exam_lungs', extractValue(dom, 'input[name=exam_lungs][checked]', 'value'));
             // * exam lungs detail: textarea#exam_lungs_detail => innerText
+            data.append('exam_lungs_detail', extractValue(dom, '#exam_lungs_detail', 'innerText'));
             // * exam abdomen: input[name=exam_abdomen][checked] => value
+            data.append('exam_abdomen', extractValue(dom, 'input[name=exam_abdomen][checked]', 'value'));
             // * exam abdomen detail: textarea#exam_abdomen_detail => innerText
+            data.append('exam_abdomen_detail', extractValue(dom, '#exam_abdomen_detail', 'innerText'));
             // * exam Extremities: input[name=exam_extrem][checked] => value
+            data.append('exam_extremities', extractValue(dom, 'input[name=exam_extrem][checked]', 'value'));
             // * exam Extremities detail: textarea#exam_extremities_detail => innerText
+            data.append('exam_extremities_detail', extractValue(dom, '#exam_extremities_detail', 'innerText'));
             // * exam Nervous system: input[name=exam_nerv][checked] => value
+            data.append('exam_nervous_system', extractValue(dom, 'input[name=exam_nerv][checked]', 'value'));
             // * exam Nervous system detail: textarea#exam_nervous_detail => innerText
+            data.append('exam_nervous_system_detail', extractValue(dom, '#exam_nervous_detail', 'innerText'));
             // * exam Lymph nodes: input[name=exam_lymph][checked] => value
+            data.append('exam_lymph_nodes', extractValue(dom, 'input[name=exam_lymph][checked]', 'value'));
             // * exam Lymph nodes detail: textarea#exam_lymph_detail => innerText
+            data.append('exam_lymph_nodes_detail', extractValue(dom, '#exam_lymph_detail', 'innerText'));
             // * exam breasts: input[name=exam_breasts][checked] => value
+            data.append('exam_breasts', extractValue(dom, 'input[name=exam_breasts][checked]', 'value'));
             // * exam breasts detail: textarea#exam_breasts_detail => innerText
+            data.append('exam_breasts_detail', extractValue(dom, '#exam_breasts_detail', 'innerText'));
             // * exam genitalia: input[name=exam_genitalia][checked] => value
+            data.append('exam_genitalia', extractValue(dom, 'input[name=exam_genitalia][checked]', 'value'));
             // * exam genitalia detail: textarea#exam_genitalia_detail => innerText
+            data.append('exam_genitalia_detail', extractValue(dom, '#exam_genitalia_detail', 'innerText'));
             // * exam rectal: input[name=exam_rectal][checked] => value
+            data.append('exam_rectal', extractValue(dom, 'input[name=exam_rectal][checked]', 'value'));
             // * exam rectal detail: textarea#exam_rectal_detail => innerText
+            data.append('exam_rectal_detail', extractValue(dom, '#exam_rectal_detail', 'innerText'));
             // * Pertinent investigation: textarea#pertinent => innerText
+            data.append('pertinent_investigation', extractValue(dom, '#pertinent', 'innerText'));
             // * Problem list: textarea#problem => innerText
+            data.append('problem_list', extractValue(dom, '#problem', 'innerText'));
             // * Problem list continue: textarea#problem_cont => innerText
+            data.append('problem_list_continue', extractValue(dom, '#problem_cont', 'innerText'));
             // * discussion: textarea#discussion => innerText
+            data.append('discussion', extractValue(dom, '#discussion', 'innerText'));
             // * provisional_diagnosis: textarea#provisional_diag => innerText
+            data.append('provisional_diagnosis', extractValue(dom, '#provisional_diag', 'innerText'));
             // * Plan of investigation: textarea#plan_invest => innerText
+            data.append('plan_investigation', extractValue(dom, '#plan_invest', 'innerText'));
             // * Plan of management: textarea#plan_manage => innerText
+            data.append('plan_management', extractValue(dom, '#plan_manage', 'innerText'));
             // * plan_special_group: input#plan_special_group => checked
+            data.append('plan_special_group', extractValue(dom, '#plan_special_group', 'checked'));
             // * CPG_detail: input#CPG_detail => value
+            data.append('CPG_detail', extractValue(dom, '#CPG_detail', 'value'));
             // * Plan of consultation: textarea#plan_consult => innerText
+            data.append('plan_consultation', extractValue(dom, '#plan_consult', 'innerText'));
             // * can estimate los: input[name=estimated] => checked
+            data.append('can_estimate_los', extractValue(dom, 'input[name=estimated]', 'checked'));
             // * estimated los: input#length_stay => value
-
+            data.append('estimated los', extractValue(dom, '#length_stay', 'value'));
+            return data;
         }
 
         function extractValue(node, query, attribute, isSelect = false) {
