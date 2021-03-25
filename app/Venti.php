7f1fd7d278381ed37b91f2062ec480bf28296160
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-25 23:19:34 +0700

    fix typo

--- app/Venti.php
+++ app/Venti.php
@@ -8,13 +8,13 @@ class Venti
     {
         $patients = [];
 
-        $countP = count($ps);
+        $countP = count($span);
         $i = 0;
         while ($i < $countP) {
             $patients[] = [
-                'bed' => $ps[$i],
-                'hn' => str_replace('HN', '', $ps[$i + 2]),
-                'name' => $ps[$i + 1],
+                'bed' => $span[$i],
+                'hn' => str_replace('HN', '', $span[$i + 2]),
+                'name' => $span[$i + 1],
             ];
             $i += 4;
         }
