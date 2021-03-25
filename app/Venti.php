1b818fcbada3ab93203c53f0406a1dfd2505160d
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-25 23:29:04 +0700

    ad debug

--- app/Venti.php
+++ app/Venti.php
@@ -10,13 +10,14 @@ class Venti
 
         $countP = count($spans) - 1;
         $i = 0;
-        while ($i <= $countP) {
+        while ($i < $countP) {
             $patients[] = [
                 'bed' => $spans[$i],
                 'hn' => str_replace('HN', '', $spans[$i + 2]),
                 'name' => $spans[$i + 1],
             ];
             $i += 4;
+            \Log::debug($i);
         }
 
         \Log::info($patients);
