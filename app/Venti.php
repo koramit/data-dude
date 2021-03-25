e788c8d95f11694de1f782be5ab2dfedc98c7f42
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-25 23:34:16 +0700

    gotcha

--- app/Venti.php
+++ app/Venti.php
@@ -11,13 +11,22 @@ class Venti
         $countP = count($spans) - 1;
         $i = 0;
         while ($i < $countP) {
-            $patients[] = [
-                'bed' => $spans[$i],
-                'hn' => str_replace('HN', '', $spans[$i + 2]),
-                'name' => $spans[$i + 1],
-            ];
-            $i += 4;
-            \Log::debug($i);
+            if (is_numeric($spans[$i])) {
+                $patients[] = [
+                    'bed' => $spans[$i],
+                    'hn' => str_replace('HN', '', $spans[$i + 2]),
+                    'name' => $spans[$i + 1],
+                ];
+                $i += 4;
+            } else {
+                $patients[] = [
+                    'bed' => null,
+                    'hn' => str_replace('HN', '', $spans[$i + 1]),
+                    'name' => $spans[$i],
+                ];
+                $i += 3;
+            }
+            // \Log::debug($i);
         }
 
         \Log::info($patients);
