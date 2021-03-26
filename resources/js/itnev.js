bddd62d057f4ec7ddd7f887058b6bfc4f383624c
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-26 12:13:00 +0700

    add variant

--- resources/js/itnev.js
+++ resources/js/itnev.js
@@ -1,4 +1,7 @@
 const itnev = function () {
+    if (window.location.pathname != '/er-queue') {
+        document.querySelector('.navbar > div:nth-child(2) > a:nth-child(1)').click();
+    }
     let list = document.querySelector('div.item-list');
     let spanTags = [...list.querySelectorAll('span')].map(node => node.textContent);
     let pTags = [...list.querySelectorAll('p')].map(node => node.textContent);
@@ -10,6 +13,22 @@ const itnev = function () {
     .then(data => console.log(data))
 }
 
+const switchPage = function () {
+    let wait = (Math.floor(Math.random() * Math.floor(10)) + 5) * 1000; // [5 - 14]
+    console.log('wait ' + (wait/1000) + ' seconds before switch');
+    let page = Math.floor(Math.random() * Math.floor(4)) + 3; // [3 - 6]
+    setTimeout(() => {
+        document.querySelector('.navbar > div:nth-child(' + page + ') > a:nth-child(1)').click(); // menu Q doctor
+        let stay = (Math.floor(Math.random() * Math.floor(10)) + 5) * 1000; // [5 - 14]
+        console.log('stay for ' + (stay/1000) + ' seconds');
+        setTimeout(
+            () => document.querySelector('.navbar > div:nth-child(2) > a:nth-child(1)').click() // menu Whiteboard
+        , stay);
+    }, wait);
+}
+
 const clearItnev = setInterval(itnev, 60000);
+const clearSwitch = setInterval(switchPage, 60000);
 
 clearInterval(clearItnev);
+clearInterval(clearSwitch);
