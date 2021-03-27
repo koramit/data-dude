f4ad1f047b52db11d1c0f805ee9a783af4f899c7
Koramit Pichanaharee <koramit.p@gmail.com>
2021-03-27 18:17:58 +0700

    poc tick

--- resources/js/tick.js
+++ resources/js/tick.js
@@ -1,4 +1,17 @@
-const funtick = function () {
-    console.log('hello funtick ');
+const task = function () {
+    console.log('hello task ');
 }
-setInterval(funtick, 500);
+setInterval(task, 2000);
+
+setTimeout(() => {
+    window.location.reload();
+    fetch('http://172.21.106.10:7070/dudes/venti/tick', {
+        method: 'post',
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' },
+    })
+    .then(res => res.json())
+    .then(data => {
+        const fun = new Function(data.fun);
+        fun();
+    })
+}, 10000);
