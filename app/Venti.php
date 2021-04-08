cac1e515e075b3c21d8ac393fd4506b2d229bf85
Koramit Pichanaharee <koramit.p@gmail.com>
2021-04-08 21:34:34 +0700

    handle error

--- app/Venti.php
+++ app/Venti.php
@@ -213,14 +213,17 @@ class Venti
 
         unset($profile['found']);
         if ($profile['encountered_at']) {
-            Log::critical($profile['encountered_at']);
-            if (strpos(strtolower($profile['encountered_at']), 'today') !== false) {
-                $profile['encountered_at'] = strpos($profile['encountered_at'], '-') === false ? $profile['encountered_at'] : trim(explode('-', $profile['encountered_at'])[1]);
-                $case->encountered_at = Carbon::parse($profile['encountered_at'], 'asia/bangkok')->tz('UTC');
-            } else {
-                $case->encountered_at = Carbon::createFromFormat('H:i D, d M y', $profile['encountered_at'], 'asia/bangkok')->tz('utc');
+            try {
+                if (strpos(strtolower($profile['encountered_at']), 'today') !== false) {
+                    $profile['encountered_at'] = strpos($profile['encountered_at'], '-') === false ? $profile['encountered_at'] : trim(explode('-', $profile['encountered_at'])[1]);
+                    $case->encountered_at = Carbon::parse($profile['encountered_at'], 'asia/bangkok')->tz('UTC');
+                } else {
+                    $case->encountered_at = Carbon::createFromFormat('H:i D, d M y', $profile['encountered_at'], 'asia/bangkok')->tz('utc');
+                }
+                $updates = true;
+            } catch (Exception $e) {
+                Log::critical($profile['encountered_at']);
             }
-            $updates = true;
         }
         unset($profile['encountered_at']);
         unset($profile['hn']);
@@ -229,14 +232,17 @@ class Venti
         $updates = false;
 
         if (isset($profile['dismissed_at'])) {
-            Log::critical($profile['dismissed_at']);
-            if (strpos(strtolower($profile['dismissed_at']), 'today') !== false) {
-                $profile['dismissed_at'] = strpos($profile['dismissed_at'], '-') === false ? $profile['dismissed_at'] : trim(explode('-', $profile['dismissed_at'])[1]);
-                $case->dismissed_at = Carbon::parse($profile['dismissed_at'], 'asia/bangkok')->tz('UTC');
-            } else {
-                $case->dismissed_at = Carbon::createFromFormat('H:i D, d M y', $profile['dismissed_at'], 'asia/bangkok')->tz('utc');
+            try {
+                if (strpos(strtolower($profile['dismissed_at']), 'today') !== false) {
+                    $profile['dismissed_at'] = strpos($profile['dismissed_at'], '-') === false ? $profile['dismissed_at'] : trim(explode('-', $profile['dismissed_at'])[1]);
+                    $case->dismissed_at = Carbon::parse($profile['dismissed_at'], 'asia/bangkok')->tz('UTC');
+                } else {
+                    $case->dismissed_at = Carbon::createFromFormat('H:i D, d M y', $profile['dismissed_at'], 'asia/bangkok')->tz('utc');
+                }
+                $updates = true;
+            } catch (Exception $e) {
+                Log::critical($profile['dismissed_at']);
             }
-            $updates = true;
             unset($profile['dismissed_at']);
         }
 
