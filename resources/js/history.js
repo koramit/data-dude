c405c9a676a111266688ebdf2277af816e018852
Koramit Pichanaharee <koramit.p@gmail.com>
2021-04-08 19:56:58 +0700

    discrete

--- resources/js/history.js
+++ resources/js/history.js
@@ -59,11 +59,11 @@ const searchHistory = async function(stay) {
         dateRef = new Date(stay.timestamp);
         dateStart = new Date(firstDate + ' ' + firstTime);
         if (dateRef < dateStart) { // next
-            pageNo += (parseInt((maxPage - pageNo) / 2) !== 0 ? parseInt((maxPage - pageNo) / 2) : 1);
             minPage = pageNo + 1;
+            pageNo += (parseInt((maxPage - pageNo) / 2) !== 0 ? parseInt((maxPage - pageNo) / 2) : 1);
         } else { // previous
-            pageNo -= (parseInt((pageNo - minPage) / 2) !== 0 ? parseInt((pageNo - minPage) / 2) : 1);
             maxPage = pageNo - 1;
+            pageNo -= (parseInt((pageNo - minPage) / 2) !== 0 ? parseInt((pageNo - minPage) / 2) : 1);
         }
         pages[pageNo].click();
         await sleep(6000);
